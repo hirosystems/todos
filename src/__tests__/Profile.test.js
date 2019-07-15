c5f583e3ec5da55ab036ed47fe0bd8b9a693a1f1
Sahana Srinivasan <sahanas009@gmail.com>
2019-07-15 11:58:36 -0400

    changed Dashboard to Profile in unit tests

--- src/__tests__/Profile.test.js
+++ src/__tests__/Profile.test.js
@@ -6,7 +6,7 @@ import * as blockstack from 'blockstack'
 // InstanceDataStore not exported from blockstack.ja by default; 
 // must import using full path
 import { InstanceDataStore } from 'blockstack/lib/auth/sessionStore'
-import Dashboard from '../components/Dashboard.js'
+import Profile from '../components/Profile.js'
 import React from 'react';
 import {configure, mount, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -35,7 +35,7 @@ const userSession = new blockstack.UserSession({
 
 // Testing that to-do items are rendered
 test('renders to-do list text', () => {
-  const wrapper = shallow(<Dashboard userSession={userSession} />);
+  const wrapper = shallow(<Profile userSession={userSession} />);
   wrapper.setState({tasks: [['Create a Blockstack ID', false]]});
   const text = wrapper.find('.task');
   expect(text.text()).toBe('Create a Blockstack ID');
